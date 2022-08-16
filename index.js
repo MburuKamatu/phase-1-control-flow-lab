6a8d0e70429d883d1713d5b7f363b33ddaa29c29
+ <+>
2022-08-17 00:07:47 +0300

    Control flow

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if(someValue<=400){
+    return 'This one is on me!';
+  }else if(someValue>2499){
+    return 'No can do.';
+  }else if(someValue>=2000){
+  return 'I will gladly take your thirty bucks.';
+}
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city==='NYC'?"Ok, sounds good.":"No go."
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip){
+    case'generous':
+    return "Thank you so much.";
+    break;
+    case'not as generous':
+    return "Thank you."
+    break;
+    case'thanks for everything':
+    return "Bye.";
+    break
+  }
 }
